4afe44bb6472267efe0cac4882ad764efeeda5f1
Aiyern30 <ianbian2@gmail.com>
2025-02-18 21:21:05 +0800

    added darktheme design mode for ConnectButton

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "4f69cd9e3add96465fdfd34b55057537",
   chains: [mainnet, polygon, optimism, arbitrum, base],
   ssr: true,
 });
@@ -19,7 +23,17 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#7b3fe4",
+            accentColorForeground: "white",
+            borderRadius: "small",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export default function Home() {
   return (
     <div>
-      <ConnectButton />
+      <ConnectButton
+        accountStatus={{
+          smallScreen: "avatar",
+          largeScreen: "full",
+        }}
+      />
     </div>
   );
 }
