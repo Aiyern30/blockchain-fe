c56f7d95c8a85608b5b7f15375d30f0d46a04c9c
xInr0u <xinrou1961@gmail.com>
2025-04-28 18:21:52 +0800

    modified viewListedNFT, able to display now

--- lib/marketplaceConfig.ts
+++ lib/marketplaceConfig.ts
@@ -5,12 +5,18 @@ import { ethers } from "ethers";
 const marketplaceAddress = "0xd637898a14E20211BFD466f1b3f8A67cbDBf748E"; // your marketplace contract
 
 const marketplaceABI = [
-  "function fetchMarketItems() view returns (tuple(address collection, uint256 tokenId, uint256 price, address seller)[])",
+  "function fetchMarketItems() view returns (tuple(address collection, uint256 tokenId, uint256 price, address seller, address owner, bool sold)[])",
   "function listNFT(address collection, uint256 tokenId, uint256 price) external payable",
   "function buyNFT(address collection, uint256 tokenId) external payable",
 ];
 
 // This is for your marketplace
-export function getMarketplaceContract(signerOrProvider: ethers.Signer | ethers.Provider) {
-  return new ethers.Contract(marketplaceAddress, marketplaceABI, signerOrProvider);
+export function getMarketplaceContract(
+  signerOrProvider: ethers.Signer | ethers.Provider
+) {
+  return new ethers.Contract(
+    marketplaceAddress,
+    marketplaceABI,
+    signerOrProvider
+  );
 }
