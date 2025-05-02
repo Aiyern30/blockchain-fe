6b0f9a0ea23938e7916f7d22d35e0c2cbd6a9964
Aiyern30 <ianbian2@gmail.com>
2025-05-02 10:52:12 +0800

    `Update ERC721 contract ABI and add new function to utils/function.ts`

--- utils/function.ts
+++ utils/function.ts
@@ -2,20 +2,3 @@ export const formatAddress = (address?: string) => {
   if (!address) return "Not Connected";
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 };
-
-export const getIpfsUrl = (cidOrUrl?: string): string => {
-  if (!cidOrUrl) return "/placeholder.svg";
-
-  if (cidOrUrl.startsWith("http")) return cidOrUrl;
-
-  if (cidOrUrl.startsWith("ipfs://")) {
-    cidOrUrl = cidOrUrl.replace("ipfs://", "");
-  }
-
-  return `https://dweb.link/ipfs/${cidOrUrl}`;
-};
-
-export function extractCID(url: string): string {
-  const match = url.match(/(?:ipfs:\/\/|https?:\/\/.*?\/ipfs\/)([a-zA-Z0-9]+)/);
-  return match ? match[1] : url;
-}
