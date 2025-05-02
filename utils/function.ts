77a70b1fe1b97363ce1487382512d462af0dea42
Aiyern30 <ianbian2@gmail.com>
2025-05-02 10:53:08 +0800

    `Moved image URL formatting and address truncation functions to utils/function.ts`

--- utils/function.ts
+++ utils/function.ts
@@ -1,4 +1,18 @@
-export const formatAddress = (address?: string) => {
-  if (!address) return "Not Connected";
+export const formatImageUrl = (imageUrl: string) => {
+  if (!imageUrl) return "/placeholder.svg";
+
+  if (imageUrl.startsWith("ipfs://")) {
+    return `https://ipfs.io/ipfs/${imageUrl.replace("ipfs://", "")}`;
+  }
+
+  if (imageUrl.includes("gateway.pinata.cloud/ipfs/")) {
+    return imageUrl;
+  }
+
+  return imageUrl;
+};
+
+export const truncateAddress = (address: string) => {
+  if (!address) return "";
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 };
