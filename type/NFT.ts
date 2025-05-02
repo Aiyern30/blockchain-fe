f21da78243b77570ba5f483ee5681c3b9cee30df
Aiyern30 <ianbian2@gmail.com>
2025-05-02 15:47:17 +0800

    `Removed unused NFTAttribute interface and FetchedNFT and Collection interfaces`

--- type/NFT.ts
+++ type/NFT.ts
@@ -1,29 +1,7 @@
-export interface NFTAttribute {
-  trait_type: string;
-  value: string;
-}
-
 export interface NFTMetadata {
   name: string;
   description: string;
   image: string;
   external_url?: string;
   attributes?: { trait_type: string; value: string }[];
-}
-
-export interface FetchedNFT {
-  id?: number;
-  cid?: string;
-  title: string;
-  image: string;
-  floor: string;
-  owner?: string;
-}
-
-export interface Collection {
-  name: string;
-  description?: string;
-  floorPrice: string;
-  image: string;
-  id: string;
 }
